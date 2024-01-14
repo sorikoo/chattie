dd34fbf8703b6029cc3c92b183fc95b4103c32ea
Ifnan Fikadu <ifnanfikadu123@gmail.com>
2024-01-14 14:46:53 +0300

    first repo

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.chatbar}> 
+  <View style={styles.profile}/>
+        <View>
+          <Text style={styles.text}>Kira</Text>
+      <Text style={styles.text}>kira@emai</Text>
+        </View>
+        
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +20,32 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:10,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  text:{
+    fontSize: 20,
+    paddingLeft:5
+  },
+  chatbar:{
+    justifyContent:'flex-start',
+    alignItems:'center',
+    backgroundColor:'dogerblue',
+    margin:20,
+    borderRadius:20,
+    borderWidth: 4,
+    padding:10,
+    width:'99%',
+    flexDirection:'row',
+    gap: 2
+  },
+  profile:{
+    width:50,
+    height:50,
+    backgroundColor:'red',
+    borderRadius:25,
+  }
 });
